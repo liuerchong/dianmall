c47cec3cbd5ded3a6610897f2ec84a6738d35618
lx <lx@LAPTOP-ML8IAD5Q>
2020-06-23 15:51:34 +0800

    代码生成

---
+++ dianmall_project_generate/src/main/resources/template/dao/Mapper.java
@@ -0,0 +1,13 @@
+package ${package_mapper};
+import ${package_pojo}.${Table};
+import tk.mybatis.mapper.common.Mapper;
+
+
+/****
+ * @Author:liuerchong
+ * @Description:
+ * @Date 2020/6/01 10:18
+ *****/
+public interface ${Table}Mapper extends Mapper<${Table}> {
+	
+}
